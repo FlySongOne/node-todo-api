71c877ffe4729da01eb9072d9f4cd1ac8414b019
Kyung Han Song <kyungsong@Kyungs-Air.fios-router.home>
2017-12-30 03:23:42 -0500

    add Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,8 +3,9 @@
 var mongoose = require('mongoose');
 // mongoose configured
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI );
 
 module.exports = {
   mongoose: mongoose   // it can be just mongoose in es 6 
 }
+
