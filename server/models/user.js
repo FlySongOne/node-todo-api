d429f2eccf172c17edc043d0a0285b9fcbcc11e1
Kyung Han Song <kyungsong@Kyungs-Air.fios-router.home>
2018-01-06 19:44:42 -0500

    add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -75,6 +75,27 @@ UserSchema.statics.findByToken = function (token){
 
   }); 
 }; 
+
+UserSchema.statics.findByCredentials = function (email, password) {
+  var User = this;
+
+  return User.findOne({email}).then((user) => {
+    if(!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      // use bcypt.compare to compare password and user.password
+      bcrypt.compare(password, user.password, (err, res) => {
+        if( res ){
+          resolve(user);
+        }else{
+          reject();
+        }
+      });
+    });
+  });
+};
  // this will run some code before a given event: we want to run save event
  // before we save, we want to make some changes to it. 
 UserSchema.pre('save', function (next){
